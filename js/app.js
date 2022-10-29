304549413abf89aef2ffb2ab235a9a38b6032586
Oscar Cantor <ocantorb1889@gmail.com>
2022-10-29 06:29:22 +0200

    Sehacen pruebas boton añadir clase

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 // VARIABLES
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+  // Cuando se agrega un curso presionando btn "Agregar al Carrito"
+  listaCursos.addEventListener('click', agregarCurso);
+
+};
+
+// FUNCIONES
+function agregarCurso(e) {
+  e.preventDefault();
+  if( e.target.classList.contains( 'agregar-carrito' ) ) {
+    console.log( 'Si tiene la clase agregar-carrito' );
+  } else {
+    console.log('No tiene la clase agregar-carrito');
+  }  
+}
 
